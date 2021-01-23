99826578b54578d1573f5d09ed1dacea7d7d23ce
nlaurance <nicolaslaurance@python.ie>
2021-01-23 18:24:41 +0000

    unit test traceback

--- tests/test_teams_logger.py
+++ tests/test_teams_logger.py
@@ -3,7 +3,7 @@ import json
 import sys
 import time
 import unittest.mock
-from logging import Handler, INFO, WARNING, getLogger, LogRecord, shutdown
+from logging import Handler, INFO, ERROR, WARNING, getLogger, LogRecord, shutdown
 from logging.config import dictConfig
 
 from teams_logger import TeamsHandler, TeamsQueueHandler, Office365CardFormatter
@@ -103,7 +103,7 @@ class TestOffice365CardFormatter(unittest.TestCase):
         exc_info = FakeException, None, tb
 
         log_record = LogRecord(
-            name="logger", level=INFO,
+            name="logger", level=ERROR,
             pathname=__name__, lineno=1, msg="hello %s",
             args=("world",), exc_info=exc_info)
 
@@ -112,7 +112,7 @@ class TestOffice365CardFormatter(unittest.TestCase):
         expected = {
             "@context": "https://schema.org/extensions",
             "@type": "MessageCard",
-            "title": "Info in test_teams_logger",
+            "title": "Error in test_teams_logger",
             "summary": "hello world",
             "sections": [{"facts": self.expected_facts_in_message_card}],
             "themeColor": "#008000",
